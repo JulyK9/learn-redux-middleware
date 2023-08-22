bc48eaa584fe56258963ccce59e1598bd4e2e406
JKKIM <jkesonjk@gmail.com>
2023-08-22 20:53:08 +0900

    fix: 커스텀 로거 비활성화

--- src/modules/store.js
+++ src/modules/store.js
@@ -1,5 +1,10 @@
 import { createStore, applyMiddleware } from 'redux';
 import rootReducer from './index';
-import myLogger from '../middlewares/myLogger';
+// import myLogger from '../middlewares/myLogger';
+import logger from 'redux-logger';
 
-export const store = createStore(rootReducer, applyMiddleware(myLogger));
+export const store = createStore(
+  rootReducer,
+  // applyMiddleware(myLogger, logger)
+  applyMiddleware(logger)
+);
