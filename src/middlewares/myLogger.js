604d8529b23c8ee0e8e01a4d8b48d3914e5f5341
JKKIM <jkesonjk@gmail.com>
2023-08-22 19:03:50 +0900

    feat: 미들웨어 적용 및 로깅에 상태조회 확인 추가

--- src/middlewares/myLogger.js
+++ src/middlewares/myLogger.js
@@ -1,6 +1,12 @@
 const myLogger = (store) => (next) => (action) => {
-  console.log(action); // 액션 출력
+  console.log('action: ', action); // 액션 출력
   const result = next(action); // 다음 미들웨어에게 액션을 전달
+
+  console.log('result: ', result); // 확인용
+
+  // 업데이트 이후 상태 조회
+  console.log('\t', store.getState()); // '\t' 는 탭 문자
+
   return result; // 반환되는 값은 dispatch(action)의 결과물이 됨 (기본: undefined)
 };
 
--- src/modules/store.js
+++ src/modules/store.js
@@ -1,4 +1,5 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import rootReducer from './index';
+import myLogger from '../middlewares/myLogger';
 
-export const store = createStore(rootReducer);
+export const store = createStore(rootReducer, applyMiddleware(myLogger));
