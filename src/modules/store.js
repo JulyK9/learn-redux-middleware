39395e2d71df16738b556fdf9d105fda74de4f43
JKKIM <jkesonjk@gmail.com>
2023-08-22 20:58:41 +0900

    feat: redux devtools 패키지 설치 및 적용

--- src/modules/store.js
+++ src/modules/store.js
@@ -1,4 +1,5 @@
 import { createStore, applyMiddleware } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
 import rootReducer from './index';
 // import myLogger from '../middlewares/myLogger';
 import logger from 'redux-logger';
@@ -6,5 +7,5 @@ import logger from 'redux-logger';
 export const store = createStore(
   rootReducer,
   // applyMiddleware(myLogger, logger)
-  applyMiddleware(logger)
+  composeWithDevTools(applyMiddleware(logger))
 );
