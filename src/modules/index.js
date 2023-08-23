d2820981cf8e8990da0f9c58c57304b6304cfefc
JKKIM <jkesonjk@gmail.com>
2023-08-23 23:05:31 +0900

    feat: posts 슬라이스 모듈을 루트 리듀서에 등록

--- src/modules/index.js
+++ src/modules/index.js
@@ -1,6 +1,10 @@
 import { combineReducers } from 'redux';
 import counter from './couter';
+import posts from './posts';
 
-const rootReducer = combineReducers({ counter });
+const rootReducer = combineReducers({
+  counter: counter,
+  posts: posts,
+});
 
 export default rootReducer;
