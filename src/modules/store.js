2abc89a138e97d7a317ceb5cddef685d35c44a43
JKKIM <jkesonjk@gmail.com>
2023-08-23 15:54:02 +0900

    feat: thunk 미들웨어 적용

--- src/modules/store.js
+++ src/modules/store.js
@@ -3,9 +3,12 @@ import { composeWithDevTools } from 'redux-devtools-extension';
 import rootReducer from './index';
 // import myLogger from '../middlewares/myLogger';
 import logger from 'redux-logger';
+import ReduxThunk from 'redux-thunk';
 
 export const store = createStore(
   rootReducer,
   // applyMiddleware(myLogger, logger)
-  composeWithDevTools(applyMiddleware(logger))
+  // logger를 사용할 경우 logger가 가장 마지막에 와야함
+  composeWithDevTools(applyMiddleware(ReduxThunk, logger))
+  //
 );
