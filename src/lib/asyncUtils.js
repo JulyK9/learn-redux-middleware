624a447c2f1cff685f84c329f63faebe07a14b2b
JKKIM <jkesonjk@gmail.com>
2023-08-25 16:07:50 +0900

    chore: promise thunk 로직 설명 추가

--- src/lib/asyncUtils.js
+++ src/lib/asyncUtils.js
@@ -1,10 +1,15 @@
 // Promise에 기반한 Thunk를 만들어주는 함수
+// 디스패치하는 액션의 타입명 ("어떤 요청"에 대한 로딩,성공,실패인지만 다르고 로딩,성공,실패 자체는 동일하고
+// 호출하는 api만 변하고 나머지는 동일하기 때문에,
+// 변화하는 부분만 파라미터 (type, promiseCreator) 로 받아서 thunk 함수를 생성해주는 함수임
 export const createPromiseThunk = (type, promiseCreator) => {
   const [SUCCESS, ERROR] = [`${type}_SUCCESS`, `${type}_ERROR`];
 
   // 이 함수는 promiseCreator가 단 하나의 파라미터만 받는다는 전제하에 작성됨
   // 만약 여러 종류의 파라미터를 전달해야 하는 상황이라면 객체 타입의 파라미터를 받아오도록 하면 됨
   // 예시: writeComment({ postId: 1, text: '댓글 내용' })
+  // getPostsById 같은 경우 어떤 포스트를 조회할 것인지 id 라는 파라미터로 식별을 하기때문에
+  // api 호출에서 필요에 따라 파라미터를 넣어줘야하는 경우도 있으므로 그것을 param으로 받는것
   return (param) => async (dispatch) => {
     dispatch({ type, param }); // 요청 시작
     console.log('param: ', param);
